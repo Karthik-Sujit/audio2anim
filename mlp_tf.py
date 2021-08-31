21eced89fda59b0f37610dbcda770259d6d815a5
Karthik <sk30102k@gmail.com>
2021-08-31 18:54:01 +0530

    docstring changes

--- mlp_tf.py
+++ mlp_tf.py
@@ -8,22 +8,37 @@ from random import random
 
 
 def generate_dataset(num_samples, test_size=0.33):
-    """Generates train/test data for sum operation
-    :param num_samples (int): Num of total samples in dataset
-    :param test_size (int): Ratio of num_samples used as test set
-    :return x_train (ndarray): 2d array with input data for training
-    :return x_test (ndarray): 2d array with input data for testing
-    :return y_train (ndarray): 2d array with target data for training
-    :return y_test (ndarray): 2d array with target data for testing
+    """Generates train/test dataset for sum operation
+
+    Args:
+        num_samples (int): Num of total samples in dataset
+        test_size (float, optional): Ratio of output to num_samples. Defaults to 0.33.
+
+    Returns:
+        x_train: 2d array with input data for training
+        x_test: 2d array with input data for testing
+        y_train: 2d array with target data for training
+        y_test: 2d array with target data for testing
     """
 
+    # """Generates train/test data for sum operation
+    # :param num_samples (int): Num of total samples in dataset
+    # :param test_size (int): Ratio of num_samples used as test set
+    # :return x_train (ndarray): 2d array with input data for training
+    # :return x_test (ndarray): 2d array with input data for testing
+    # :return y_train (ndarray): 2d array with target data for training
+    # :return y_test (ndarray): 2d array with target data for testing
+    # """
+
     # build inputs/targets for sum operation: y[0][0] = x[0][0] + x[0][1]
-    x = np.array([[random()/2 for _ in range(2)] for _ in range(num_samples)])
+    x = np.array([[random() / 2 for _ in range(2)]
+                 for _ in range(num_samples)])
     y = np.array([[i[0] + i[1]] for i in x])
 
     # split dataset into test and training sets
     x_train, x_test, y_train, y_test = train_test_split(
         x, y, test_size=test_size)
+
     return x_train, x_test, y_train, y_test
 
 
@@ -33,10 +48,12 @@ x_train, x_test, y_train, y_test = generate_dataset(5000, 0.3)
 
 # %%
 # build model with 3 layers: 2 -> 5 -> 1
-model = tf.keras.models.Sequential([
-    tf.keras.layers.Dense(5, input_dim=2, activation="sigmoid"),
-    tf.keras.layers.Dense(1, activation="sigmoid")
-])
+model = tf.keras.models.Sequential(
+    [
+        tf.keras.layers.Dense(5, input_dim=2, activation="sigmoid"),
+        tf.keras.layers.Dense(1, activation="sigmoid"),
+    ]
+)
 
 # %%
 # choose optimizer
@@ -44,7 +61,7 @@ optimizer = tf.keras.optimizers.SGD(learning_rate=0.1)
 
 # %%
 # compile model
-model.compile(optimizer=optimizer, loss='mse')
+model.compile(optimizer=optimizer, loss="mse")
 
 # %%
 # train model
