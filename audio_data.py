8d106f003a961bdb79bbcc891fde345956c4a936
Karthik <sk30102k@gmail.com>
2021-09-11 18:31:24 +0530

    formatting with yapf

--- audio_data.py
+++ audio_data.py
@@ -50,8 +50,8 @@ fft = np.fft.fft(signal)
 magnitude = np.abs(fft)
 frequency = np.linspace(0, sr, len(magnitude))
 
-left_frequency = frequency[: int(len(frequency) / 2)]
-left_magnitude = magnitude[: int(len(magnitude) / 2)]
+left_frequency = frequency[:int(len(frequency) / 2)]
+left_magnitude = magnitude[:int(len(magnitude) / 2)]
 
 # %%
 # plt.plot(left_frequency, left_magnitude)
@@ -78,7 +78,10 @@ librosa.display.specshow(log_spectrogram, sr=sr, hop_length=hop_length)
 
 # %%
 # MFCCs
-MFCCs = librosa.feature.mfcc(signal, n_fft=n_fft, hop_length=hop_length, n_mfcc=13)
+MFCCs = librosa.feature.mfcc(signal,
+                             n_fft=n_fft,
+                             hop_length=hop_length,
+                             n_mfcc=13)
 librosa.display.specshow(MFCCs, sr=sr, hop_length=hop_length)
 plt.xlabel("Time")
 plt.ylabel("MFCCs")
--- mlp_tf.py
+++ mlp_tf.py
@@ -32,12 +32,13 @@ def generate_dataset(num_samples, test_size=0.33):
 
     # build inputs/targets for sum operation: y[0][0] = x[0][0] + x[0][1]
     x = np.array([[random() / 2 for _ in range(2)]
-                 for _ in range(num_samples)])
+                  for _ in range(num_samples)])
     y = np.array([[i[0] + i[1]] for i in x])
 
     # split dataset into test and training sets
-    x_train, x_test, y_train, y_test = train_test_split(
-        x, y, test_size=test_size)
+    x_train, x_test, y_train, y_test = train_test_split(x,
+                                                        y,
+                                                        test_size=test_size)
 
     return x_train, x_test, y_train, y_test
 
@@ -48,12 +49,10 @@ x_train, x_test, y_train, y_test = generate_dataset(5000, 0.3)
 
 # %%
 # build model with 3 layers: 2 -> 5 -> 1
-model = tf.keras.models.Sequential(
-    [
-        tf.keras.layers.Dense(5, input_dim=2, activation="sigmoid"),
-        tf.keras.layers.Dense(1, activation="sigmoid"),
-    ]
-)
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Dense(5, input_dim=2, activation="sigmoid"),
+    tf.keras.layers.Dense(1, activation="sigmoid"),
+])
 
 # %%
 # choose optimizer
